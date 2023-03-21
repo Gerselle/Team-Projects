100879fc123f2d0026c0e38744298f8414f7c040
HaroldJCromwell <77396920+HaroldJCromwell@users.noreply.github.com>
2023-03-21 08:44:25 -0400

    Delete webpage directory

--- webpage/submit.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$myfile = fopen("userpass.txt", "a") or die("Unable to open file!");
-$text = $username + "\n" + $password + "\n" + $email + "\n" + $face + "\n";
-fwrite($myfile, $text);
-fclose($myfile);
-
-?>
--- webpage/test.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo file_put_contents("C:\xampp\htdocs\Webpage2.0\newfile.txt","Hello World. Testing!");
-?>
