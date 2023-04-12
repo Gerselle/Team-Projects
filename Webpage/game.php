11f4b8229e851dd2ceaabf06fa2c9a4accc10d33
HaroldJCromwell <77396920+HaroldJCromwell@users.noreply.github.com>
2023-04-12 15:47:25 -0400

    Update game.php

---
+++ Webpage/game.php
@@ -0,0 +1,20 @@
+<?php
+
+$username = $_GET['username'];
+$password = $_GET['password'];
+$hashedPassword = hash("sha512",$password);
+
+echo $username;
+echo $password;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link  href="css/main.css"  rel="stylesheet"    >
+</head>
+</html>   
+ 
