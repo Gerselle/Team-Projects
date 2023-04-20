aae7cc7a1e142ede3eed08027952639214b09626
Gerald Sellers <sellers.34@wright.edu>
2023-04-20 06:49:46 -0400

    Updating game interface

--- Webpage/game.js
+++ Webpage/game.js
@@ -3,13 +3,14 @@ const blackjackSelect = document.querySelector('#blackjack-select');
 const pokerSelect = document.querySelector('#poker-select');
 const cardShuffle = document.querySelector('#card-shuffle');
 const readySelect = document.querySelector('#ready-select');
+const lobbyGamestate = document.querySelector('#lobby-gamestate');
 // Blackjack elements
 const dealerHand = document.querySelector("#dealer-hand");
 const dealerScore = document.querySelector("#dealer-score");
 const blackjackHand = document.querySelector("#blackjack-hand");
-const playerScore = document.querySelector("#player-score");
+const blackjackScore = document.querySelector("#blackjack-score");
 const blackjackChips = document.querySelector("#blackjack-chips");
-const blackjackGameState = document.querySelector("#blackjack-gamestate");
+const blackjackGamestate = document.querySelector("#blackjack-gamestate");
 // Blackjack player options
 const blackjackInput = document.querySelector("#blackjack-input");
 const blackjackBet = document.querySelector("#blackjack-bet");
@@ -24,7 +25,9 @@ const pokerTurn = document.querySelector('#turn');
 const pokerRiver = document.querySelector('#river');
 const poker1 = document.querySelector('#poker1');
 const poker2 = document.querySelector('#poker2');
+const pokerGamestate = document.querySelector("#poker-gamestate");
 // Poker player options
+const pokerInput = document.querySelector("#poker-input");
 const betBtn = document.querySelector('#bet');
 const checkBtn = document.querySelector('#check');
 const callBtn = document.querySelector('#call');
@@ -32,74 +35,162 @@ const raiseBtn = document.querySelector('#raise');
 const foldBtn = document.querySelector('#fold');
 // Sections for each game page
 const lobby = document.querySelector('#lobby');
-const blackjack = document.querySelector('#blackjack');
-const poker = document.querySelector('#poker');
-// Connection to server
+const blackjack = document.querySelector('#Blackjack');
+const poker = document.querySelector('#Texas Holdem');
+
+
+const elements = [
+  { id: "lobby-gamestate", variable: lobbyGamestate },  
+  { id: "dealer-hand", variable: dealerHand },
+  { id: "dealer-score", variable: dealerScore },
+  { id: "blackjack-hand", variable: blackjackHand },
+  { id: "blackjack-score", variable: blackjackScore },
+  { id: "blackjack-chips", variable: blackjackChips },
+  { id: "blackjack-gamestate", variable: blackjackGamestate },
+  { id: "comm1", variable: pokerComm1 },
+  { id: "comm2", variable: pokerComm2 },
+  { id: "comm3", variable: pokerComm3 },
+  { id: "turn", variable: pokerTurn },
+  { id: "river", variable: pokerRiver },
+  { id: "poker1", variable: poker1 },
+  { id: "poker2", variable: poker2 },
+  { id: "poker-gamestate", variable: pokerGamestate }
+];
+
+const pages = [
+  { id: "lobby", variable: lobby },
+  { id: "Blackjack", variable: blackjack }
+  //{ id: "Texas Holdem", variable: poker }
+];
+
+// Connection to server and page toggles
 const username = document.getElementById("username").value;
 const socket = new WebSocket("ws://localhost:1337/");
+var gamemode = "lobby"
 
 socket.onmessage = (event) => {
-  alert(event.data);
-};
-
-socket.onopen = (event) =>{
-  lobby.style.display = "none";
-  blackjack.style.display = "block";
-  poker.style.display = "none";
-
-}
-
-// Add event listeners
-blackjackInput.addEventListener("keypress", function (event) {
-  if (event.key === "Enter") {
-    // handle input
-  }
-});
-
-blackjackBet.addEventListener("click", function () {
-  alert("Clicked!");
-  socket.send(blackjackInput.value);
-});
-
-hit.addEventListener("click", function () {
-  // handle hit
-});
-
-stand.addEventListener("click", function () {
-  // handle stand
-});
-
-double.addEventListener("click", function () {
-  // handle double
-});
-
-
-
-// Event handlers
-
-function handleReadyClick() {
-  // Check if all players are ready
-  let allReady = true;
-  playerRows.forEach((row, index) => {
-    if (index !== 0) { // Skip table header row
-      const statusCell = row.querySelector('td:last-child');
-      if (!statusCell.classList.contains('ready')) {
-        allReady = false;
+  var data = JSON.parse(event.data);
+  console.log(data)
+  
+  // Update each element in the html file to their new gamestate if applicable 
+  for (const element of elements) {
+    const value = data[element.id];
+    if (value !== undefined) {
+      if("gamestate" in data){
+          writeRows(data);
+      }
+      else if (["dealer-hand", "blackjack-hand", "community", "poker-hand"].includes(element.id)){
+        writeCards(element.id, data);
+      }
+      else{
+        element.variable.innerHTML = value;
       }
     }
-  });
-  
-  if (allReady) {
-    alert('Game starting!');
-    // Reset player statuses
-    playerRows.forEach((row, index) => {
-      if (index !== 0) {
-        const statusCell = row.querySelector('td:last-child');
-        statusCell.classList.remove('ready', 'not-ready');
-        statusCell.classList.add('not-ready');
-      }
-    });
-  } else {
-    alert('Not all players are ready yet.');
   }
+
+  // Loop through all gamemode pages and show/hide them based on the current gamemode
+  pages.forEach((page) => {
+    if (page.id == data.gamemode) {
+      page.variable.style.display = "block";
+    } else {
+      page.variable.style.display = "none";
+    }
+  });
+};
+
+// When a client load the page, they are either in the lobby 
+// or are put back into the game if they're reconnecting 
+socket.onopen = (event) =>{send("connection");}
+
+// Lobby event listeners
+blackjackSelect.addEventListener("click", function() {send("setgame", "Blackjack");});
+
+pokerSelect.addEventListener("click", function() {send("setgame", "Texas Holdem");});
+
+cardShuffle.addEventListener("click", function() {send("shuffle");});
+
+readySelect.addEventListener("click", function() {send("Ready");});
+
+
+// Blackjack event listeners
+
+blackjackBet.addEventListener("click", function() {send("bet", blackjackInput.value);});
+
+hit.addEventListener("click", function() {send("hit");});
+
+stand.addEventListener("click", function() {send("stand");});
+
+double.addEventListener("click", function() {send("double");});
+
+// Poker event listeners
+
+
+
+
+
+
+
+
+
+function writeCards(id, data){
+  var hand = data[id];
+  var cards = "";
+
+  hand.forEach((card => {
+    cards += `<div class="card"><img src="http://localhost/Htmls/PNG-cards-1.3/` + card + `.png"></div>"`
+  }));
+
+  elements[id].innerHTML = cards;
+}
+
+function writeRows(data){
+  // Variable for creating table rows might be changed based on current gamemode value
+  var rows = "<tr><th>Player</th><th>Bet</th><th>Status</th></tr>"
+  var table = lobbyGamestate;
+  switch (data["gamemode"])
+  {
+    case "lobby":
+      rows = "<tr><th>Player</th><th>Game Mode</th><th>Status</th></tr>"
+      break;
+    case "Blackjack":
+      table = blackjackGamestate;
+      break;
+    case "Texas Holdem":
+      table = pokerGamestate;
+      break;
+  }
+
+  // Variable for green/"winning" table elements, switch to red if player reached unplayable state
+  var winning = "win"
+  if(["Busted!", "Folded!", "waiting", "Lost!"].includes(player[1])){winning = "loss"}
+
+  value.forEach((player => {
+    // Highlight will invert the colors for the client's position in the table
+    var highlight = "";
+    if(player[1] == username){
+      const highlight = `class="highlighted"`; 
+    }
+    // Fill in gamestate table line by line
+    if(data["gamemode"] == "lobby"){
+      rows += "<tr" + highlight + "><td>" + player[0] + "</td><td>" + player[3] + "</td><td class='" + winning + "'>" + player[1]+ "</td></tr>";
+    }else{
+      rows += "<tr" + highlight + "><td>" + player[0] + "</td><td>" + player[2] + "</td><td class='" + winning + "'>" + player[1]+ "</td></tr>";
+    }
+  })); 
+
+  // Update the table
+  table.innerHTML = rows;
+}
+
+function send(action, additional = null) {
+  const update = {
+    action: action,
+    username: username
+  };
+
+  if(additional != null){
+    update.additional = additional;
+  }
+
+  socket.send(JSON.stringify(update));
 }
--- Webpage/game.php
+++ Webpage/game.php
@@ -1,15 +1,11 @@
 <?php
-
-//$username = $_GET['username'];
-//$password = $_GET['password'];
-//$hashedPassword = hash("sha512",$password);
+$username = $_GET['username'];
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title>It's gaming, not gambling!</title>
-    <link rel="stylesheet" type="text/css" href="css/game.css">
+    <link rel="stylesheet" type="text/css" href="game.css">
 </head>
 <body>
     <!--Lobby Section-->
@@ -18,25 +14,21 @@
             <h1>Choose a gamemode and press ready when everyone at the table is in the lobby</h1>
             <div class="lobby-options">
                 <label>
-                    <input type="checkbox" name="gameMode" value="Blackjack">
                         <button class="btn" id="blackjack-select">Blackjack</button>
-                    </input>
                 </label>
                 <label>
-                    <input type="checkbox" name="gameMode" value="Poker">
                         <button class="btn" id="poker-select">Texas Hold'em</button>
-                    </input>
                 </label>
                 <button class="btn" id="card-shuffle">Shuffle Cards</button>
                 <button class="btn" id="ready-select">Ready</button>
             </div>
-            <table class="gamestate" id="lobby">
+            <table class="gamestate" id="lobby-gamestate">
                 <tr>
                     <th>Player</th>
                     <th>Game Mode</th>
                     <th>Status</th>
                 </tr>
-                <tr>
+                <tr class="highlighted">
                     <td>Player 1</td>
                     <td>Blackjack</td>
                     <td class="win">Ready</td>
@@ -61,28 +53,29 @@
     </section>
 
     <!--Blackjack Section-->
-    <section id="blackjack">	
+    <section id="Blackjack">	
         <div class="container">
             
             <h1>Welcome to Blackjack!</h1>
             <div class="player">    
                 <div class="hand" id="dealer-hand">
-                    <div class="card"><img src="http://localhost/Htmls/PNG-cards-1.3/4_of_clubs.png"></div>
-                    <div class="card"><img src="http://localhost/Htmls/PNG-cards-1.3/4_of_clubs.png"></div>
+                    <div class="card"><img src="http://localhost/Htmls/PNG-cards-1.3/5_of_diamonds.png"></div>
+                    <div class="card"><img src="http://localhost/Htmls/PNG-cards-1.3/ace_of_clubs.png"></div>
                 </div>
-                <h1>Dealer Score: <span class="score" id="dealer-score">?</span></h1>
+                <h1>Dealer Score: <span class="score" id="dealer-score">16</span></h1>
         
                 <div class="hand" id="blackjack-hand">
                     <div class="card"><img src="http://localhost/Htmls/PNG-cards-1.3/4_of_clubs.png"></div>
-                    <div class="card"><img src="http://localhost/Htmls/PNG-cards-1.3/back_of_card.png"></div>
-                    <div class="card"><img src="http://localhost/Htmls/PNG-cards-1.3/back_of_card.png"></div>
+                    <div class="card"><img src="http://localhost/Htmls/PNG-cards-1.3/king_of_spades.png"></div>
+                    <div class="card"><img src="http://localhost/Htmls/PNG-cards-1.3/queen_of_hearts.png"></div>
                 </div>
                 <div>
-                    <h3 style="display: inline-block;">Score: <span class="score" id="player-score">?</span></h3>
-                    <h3 style="display: inline-block; margin-left: 10px;">Chips: <span class="score" id="blackjack-chips">1000</span></h3>
+                    <h3 style="display: inline-block;">Score: <span class="score" id="blackjack-score">24</span></h3>
+                    <h3 style="display: inline-block; margin-left: 10px;">Current Bet: <span class="score" id="blackjack-bet">0</span></h3>
+                    <h3 style="display: inline-block; margin-left: 10px;">Chips: <span class="score" id="blackjack-chips">900</span></h3>
                 </div>
                 <div>                   
-                     <input class="input-box" placeholder="Type a number, then press >" id="blackjack-input"></input>
+                    <input class="input-box" type="number" id="blackjack-input" min="10" max="1000" value="50" step="5"></input>
                     <button class="btn" id="blackjack-bet">Bet</button>
                     <button class="btn" id="hit">Hit</button>
                     <button class="btn" id="stand">Stand</button>
@@ -97,23 +90,23 @@
                     <th>Status</th>
                 </tr>
                 <tr>
-                    <td>Player 1</td>
-                    <td>Blackjack</td>
-                    <td class="win">Ready</td>
+                    <td>James</td>
+                    <td>$25</td>
+                    <td class="win">19</td>
                 </tr>
-                <tr>
-                    <td>Player 2</td>
-                    <td>Poker</td>
+                <tr class="highlighted">
+                    <td>Gerald</td>
+                    <td>$100</td>
                     <td class="loss">Busted!</td>
                 </tr>
                 <tr>
-                    <td>Player 3</td>
-                    <td>Blackjack</td>
-                    <td class="win">Ready</td>
+                    <td>Lloyd</td>
+                    <td>$75</td>
+                    <td class="win">Blackjack!</td>
                 </tr>
                 <tr>
-                    <td>Player 4</td>
-                    <td>Blackjack</td>
+                    <td>Brett</td>
+                    <td>$50</td>
                     <td class="loss">Busted!</td>
                 </tr>
             </table>
@@ -121,21 +114,21 @@
     </section>
 
     <!--Poker Section-->
-    <section id="poker">
+    <section id="Texas Hold'em" style="display: none;">
         <div class="container">
             <h1>Welcome to Texas Hold'em!</h1>
             <div class="player">   
-                <div class="hand">
-                    <div class="card"><img id="comm1" src="http://localhost/Htmls/PNG-cards-1.3/4_of_clubs.png"></div>
-                    <div class="card"><img id="comm2" src="http://localhost/Htmls/PNG-cards-1.3/4_of_clubs.png"></div>
-                    <div class="card"><img id="comm3" src="http://localhost/Htmls/PNG-cards-1.3/4_of_clubs.png"></div>
+                <div class="hand" id="community">
+                    <div class="card"><img id="comm1" src="http://localhost/Htmls/PNG-cards-1.3/2_of_clubs.png"></div>
+                    <div class="card"><img id="comm2" src="http://localhost/Htmls/PNG-cards-1.3/8_of_diamonds.png"></div>
+                    <div class="card"><img id="comm3" src="http://localhost/Htmls/PNG-cards-1.3/6_of_hearts.png"></div>
                     <div class="card"><img id="turn" src="http://localhost/Htmls/PNG-cards-1.3/back_of_card.png"></div>
                     <div class="card"><img id="river" src="http://localhost/Htmls/PNG-cards-1.3/back_of_card.png"></div>
                 </div> 
                 <h1>Community Cards</h1>
                 <div class="hand" id="poker-hand">
-                    <div class="card"><img src="http://localhost/Htmls/PNG-cards-1.3/4_of_clubs.png"></div>
-                    <div class="card"><img src="http://localhost/Htmls/PNG-cards-1.3/4_of_clubs.png"></div>
+                    <div class="card"><img src="http://localhost/Htmls/PNG-cards-1.3/4_of_spades.png"></div>
+                    <div class="card"><img src="http://localhost/Htmls/PNG-cards-1.3/king_of_clubs.png"></div>
                 </div> 
                 <h1>Your Hand</h1>
                 <div>
@@ -144,7 +137,7 @@
                 </div>
 
                 <div>
-                    <input class="input-box" placeholder="Type a number, then press >" id="poker-input"></input>
+                    <input class="input-box" type="number" id="poker-input" min="10" max="1000" value="50" step="5"></input>
                     <button class="btn" id="poker-bet">Bet</button>
                     <button class="btn" id="check">Check</button>
                     <button class="btn" id="call">Call</button>
@@ -159,29 +152,32 @@
                     <th>Status</th>
                 </tr>
                 <tr>
-                    <td>Player 1</td>
-                    <td>Blackjack</td>
-                    <td class="win">Ready</td>
+                    <td>James</td>
+                    <td>$25</td>
+                    <td class="win">Called</td>
                 </tr>
                 <tr>
-                    <td>Player 2</td>
-                    <td>Poker</td>
-                    <td class="loss">Busted!</td>
+                    <td>Gerald</td>
+                    <td>$100</td>
+                    <td class="loss">Folded</td>
                 </tr>
                 <tr>
-                    <td>Player 3</td>
-                    <td>Blackjack</td>
-                    <td class="win">Ready</td>
+                    <td>Lloyd</td>
+                    <td>$75</td>
+                    <td class="win">Raised by $50</td>
                 </tr>
                 <tr>
-                    <td>Player 4</td>
-                    <td>Blackjack</td>
-                    <td class="loss">Busted!</td>
+                    <td>Brett</td>
+                    <td>$50</td>
+                    <td class="loss">Folded!</td>
                 </tr>
             </table>
         </div>
     </section>
-    <input type="hidden" id="username" value="<?php echo $username ?>">
+    <!-- Hidden variables section, shouldn't be visible in viewport-->
+    <section hidden>
+        <input id="username" value="<?php echo $username ?>">
+    </section>
 </body>
 <script src="http://localhost/Htmls/game.js"></script>
 </html>
